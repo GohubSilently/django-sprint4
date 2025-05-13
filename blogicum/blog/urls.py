6dfb328a0d2a2608e7ddb64b75ceb9cd5d83b645
Vadim Khalin <khalinvadim3@gmail.com>
2025-05-13 20:20:57 +0300

    Make PostListView

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -5,8 +5,19 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
-    path('category/<slug:category_slug>/', views.category_posts,
-         name='category_posts'),
-    path('', views.index, name='index'),
+    path(
+        'posts/<int:post_id>/',
+        views.PostDetailView.as_view(),
+        name='post_detail'
+    ),
+    path(
+        'category/<slug:category_slug>/',
+        views.CategoryListView.as_view(),
+        name='category_posts'
+    ),
+    path(
+        '',
+        views.PostListView.as_view(),
+        name='index'
+    ),
 ]
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,44 +1,21 @@
 from datetime import datetime
 
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from . models import Post, Category
 
 
-def published_posts(queryset=Post.objects):
-    return queryset.select_related(
-        'category', 'author', 'location'
-    ).filter(
-        pub_date__lte=datetime.now(),
-        is_published=True,
-        category__is_published=True
-    )
+class PostListView(ListView):
+    """Display the Homepage."""
 
+    model = Post
+    template_name = 'blog/index.html'
+    paginate_by = 10
 
-def index(request):
-    return render(
-        request,
-        'blog/index.html',
-        {'posts': published_posts()[0:5]}
-    )
-
-
-def post_detail(request, post_id):
-    return render(
-        request,
-        'blog/detail.html',
-        {'post': get_object_or_404(published_posts(), pk=post_id)})
-
-
-def category_posts(request, category_slug):
-    category = get_object_or_404(
-        Category,
-        slug=category_slug,
-        is_published=True
-    )
-    posts = published_posts(category.posts)
-
-    return render(request, 'blog/category.html', {
-        'category': category,
-        'posts': posts,
-    })
+    def get_queryset(self):
+        posts = super().get_queryset()
+        return posts.filter(
+            is_published=True,
+            pub_date__lte=datetime.now()
+        )
