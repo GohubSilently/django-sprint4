2a6fe172d484ff084bf3a18162d668f285a24b45
Vadim Khalin <khalinvadim3@gmail.com>
2025-05-19 19:45:41 +0300

    bug fix

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,7 +1,6 @@
 from django.db.models import Count
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
-from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import (
     ListView,
